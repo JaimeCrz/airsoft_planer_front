fbd9dd091225f81e35f36facb5f9f9d05fa1b395
Jaime <jaime.cruz.hostalot@gmail.com>
2020-06-26 02:12:52 +0200

    Added library + Calendar in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import MainCalendar from './components/calendar'
 
 function App() {
   return (
     <div className="App">
     <p>Whello!</p>
+    <div><MainCalendar/></div>
     </div>
   );
 }
